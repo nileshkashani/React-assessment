27ad634d1192908191572007f8ccbaa9c90eb500
nilesh <nileshkashani45@gmail.com>
2026-01-01 12:47:07 +0530

    build error

--- src/firebase.js
+++ src/firebase.js
@@ -18,6 +18,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+console.log(import.meta.env)
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
